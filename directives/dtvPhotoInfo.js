3063201872d75e92a62f4ed13744a93eb7e89649
Jhon Fredy Nova Perilla <jhonfredynova@gmail.com>
2017-01-12 03:59:41 +0000

    adjust responsive

--- directives/dtvPhotoInfo.js
+++ directives/dtvPhotoInfo.js
@@ -6,7 +6,7 @@ angular.module('test.main')
             restrict: 'E',
             replace: true,
             transclude: true,
-            template: '<div class="col-sm-3 fadeIn"><div class="thumbnail"><img ng-src="{{data.thumbnailUrl}}" alt="Photo Test"><div class="caption"><h3 class="box-title">{{data.title}}</h3><p class="text-center"><a href="{{data.url}}" target="_blank" class="btn btn-success" role="button">Ver Original</a></p></div></div></div>',
+            template: '<div class="col-sm-4 col-md-3 fadeIn"><div class="thumbnail"><img ng-src="{{data.thumbnailUrl}}" alt="Photo Test"><div class="caption"><h3 class="box-title">{{data.title}}</h3><p class="text-center"><a href="{{data.url}}" target="_blank" class="btn btn-success" role="button">Ver Original</a></p></div></div></div>',
             scope: {
                 data: '=data'
             },
